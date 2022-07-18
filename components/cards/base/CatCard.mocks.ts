5f671e23a022e56d7cad493e6c7bda3fa55bd70c
Enrique Solis <esolis205@yahoo.com>
2022-07-18 16:45:09 -0700

    feat: create CatCard

---
+++ components/cards/base/CatCard.mocks.ts
@@ -0,0 +1,13 @@
+import { ICatCard } from './CatCard'
+
+const base: ICatCard = {
+    title: 'TheCatLift',
+    tag: 'gains',
+    body: 'muscles',
+    author: 'Enrique',
+    time: '22:00',
+}
+
+export const mockCatCardProps = {
+    base,
+}
